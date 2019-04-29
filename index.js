150bccf06bedc137fa5037f77d69f15d5f5d4339
xtoyx <50103636+xtoyx@users.noreply.github.com>
2019-04-29 14:26:06 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+const disco = new Discord.Client();
+let stop=false;
+
+disco.on("ready", () => {
+    disco.user.setPresence({ game: { name: `Coloring your world ${disco.users.size}` }, type: 0 });
+    console.log("Disco role bot online! Created by i am toast.");
+});
+
+disco.on("message", message => {
+   
+
+
+    if (message.content.startsWith(prefix + "a")) {
+       message.channel.send("Hello)
+        }
+       
+});
+disco.login(process.env.BOT_TOKEN);
